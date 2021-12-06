290e5cb382af51f97a11be02f11718aec91c7c5a
jeovane16 <jeovanearaujo16@gmail.com>
2021-12-06 11:19:27 -0300

    estrutura inicial dos testes

---
+++ Polen.Charity.Donation.Api.Testing/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using Polen.Charity.Donation.Api;
+
+namespace Polen.Charity.Donation.Api.Testing
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+    }
+  }
+}
+
